1e49276828e849dad86cb5b5e1700e4199c627e1
John Spounias <d3cline@pyl0n.com>
2017-06-13 16:06:36 -0600

    commit message

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceControlActivity.kt
@@ -0,0 +1,297 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.bluetoothlegatt
+
+import android.app.Activity
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattService
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.util.Log.println
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ExpandableListView
+import android.widget.SimpleExpandableListAdapter
+import android.widget.TextView
+
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * For a given BLE device, this Activity provides the user interface to connect, display data,
+ * and display GATT services and characteristics supported by the device.  The Activity
+ * communicates with `BluetoothLeService`, which in turn interacts with the
+ * Bluetooth LE API.
+ */
+class DeviceControlActivity : Activity() {
+    private var mConnectionState: TextView? = null
+    private var mDataField: TextView? = null
+    private var mDeviceName: String? = null
+    private var mDeviceAddress: String? = null
+    private var mGattServicesList: ExpandableListView? = null
+    private var mBluetoothLeService: BluetoothLeService? = null
+    private var mGattCharacteristics: ArrayList<ArrayList<BluetoothGattCharacteristic>>? = ArrayList()
+    private var mConnected = false
+    private var mNotifyCharacteristic: BluetoothGattCharacteristic? = null
+
+    private val LIST_NAME = "NAME"
+    private val LIST_UUID = "UUID"
+
+
+    // Code to manage Service lifecycle.
+    private val mServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(componentName: ComponentName, service: IBinder) {
+            mBluetoothLeService = (service as BluetoothLeService.LocalBinder).service
+            if (!mBluetoothLeService!!.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth")
+                finish()
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService!!.connect(mDeviceAddress)
+        }
+
+        override fun onServiceDisconnected(componentName: ComponentName) {
+            mBluetoothLeService = null
+        }
+    }
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
+    //                        or notification operations.
+    private val mGattUpdateReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (BluetoothLeService.ACTION_GATT_CONNECTED == action) {
+                mConnected = true
+                updateConnectionState(R.string.connected)
+                invalidateOptionsMenu()
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED == action) {
+                mConnected = false
+                updateConnectionState(R.string.disconnected)
+                invalidateOptionsMenu()
+                clearUI()
+            } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED == action) {
+                // Show all the supported services and characteristics on the user interface.
+                displayGattServices(mBluetoothLeService!!.supportedGattServices)
+            } else if (BluetoothLeService.ACTION_DATA_AVAILABLE == action) {
+                displayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA))
+            }
+        }
+    }
+
+    // If a given GATT characteristic is selected, check for supported features.  This sample
+    // demonstrates 'Read' and 'Notify' features.  See
+    // http://d.android.com/reference/android/bluetooth/BluetoothGatt.html for the complete
+    // list of supported characteristic features.
+    private val servicesListClickListner = ExpandableListView.OnChildClickListener { parent, v, groupPosition, childPosition, id ->
+        if (mGattCharacteristics != null) {
+            val characteristic = mGattCharacteristics!![groupPosition][childPosition]
+            val charaProp = characteristic.properties
+            if (charaProp or BluetoothGattCharacteristic.PROPERTY_READ > 0) {
+                // If there is an active notification on a characteristic, clear
+                // it first so it doesn't update the data field on the user interface.
+                if (mNotifyCharacteristic != null) {
+                    mBluetoothLeService!!.setCharacteristicNotification(
+                            mNotifyCharacteristic, false)
+                    mNotifyCharacteristic = null
+                }
+                mBluetoothLeService!!.readCharacteristic(characteristic)
+            }
+            if (charaProp or BluetoothGattCharacteristic.PROPERTY_NOTIFY > 0) {
+                mNotifyCharacteristic = characteristic
+                mBluetoothLeService!!.setCharacteristicNotification(
+                        characteristic, true)
+            }
+            return@OnChildClickListener true
+        }
+        false
+    }
+
+    private fun clearUI() {
+        mGattServicesList!!.setAdapter(null as SimpleExpandableListAdapter?)
+        mDataField!!.setText(R.string.no_data)
+    }
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gatt_services_characteristics)
+
+        val intent = intent
+        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME)
+        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS)
+
+        // Sets up UI references.
+        (findViewById(R.id.device_address) as TextView).text = mDeviceAddress
+        mGattServicesList = findViewById(R.id.gatt_services_list) as ExpandableListView
+        mGattServicesList!!.setOnChildClickListener(servicesListClickListner)
+        mConnectionState = findViewById(R.id.connection_state) as TextView
+        mDataField = findViewById(R.id.data_value) as TextView
+
+        //println(mDataField)
+
+        actionBar!!.title = mDeviceName
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+        val gattServiceIntent = Intent(this, BluetoothLeService::class.java)
+        bindService(gattServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter())
+        if (mBluetoothLeService != null) {
+            val result = mBluetoothLeService!!.connect(mDeviceAddress)
+            Log.d(TAG, "Connect request result=" + result)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(mGattUpdateReceiver)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(mServiceConnection)
+        mBluetoothLeService = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.gatt_services, menu)
+        if (mConnected) {
+            menu.findItem(R.id.menu_connect).isVisible = false
+            menu.findItem(R.id.menu_disconnect).isVisible = true
+        } else {
+            menu.findItem(R.id.menu_connect).isVisible = true
+            menu.findItem(R.id.menu_disconnect).isVisible = false
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_connect -> {
+                mBluetoothLeService!!.connect(mDeviceAddress)
+                return true
+            }
+            R.id.menu_disconnect -> {
+                mBluetoothLeService!!.disconnect()
+                return true
+            }
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateConnectionState(resourceId: Int) {
+        runOnUiThread { mConnectionState!!.setText(resourceId) }
+    }
+
+    private fun displayData(data: String?) {
+        if (data != null) {
+            mDataField!!.text = data
+
+            //System.out.println(data);
+        }
+    }
+
+    // Demonstrates how to iterate through the supported GATT Services/Characteristics.
+    // In this sample, we populate the data structure that is bound to the ExpandableListView
+    // on the UI.
+    private fun displayGattServices(gattServices: List<BluetoothGattService>?) {
+        if (gattServices == null) return
+        var uuid: String? = null
+        val unknownServiceString = resources.getString(R.string.unknown_service)
+        val unknownCharaString = resources.getString(R.string.unknown_characteristic)
+        val gattServiceData = ArrayList<HashMap<String, String>>()
+        val gattCharacteristicData = ArrayList<ArrayList<HashMap<String, String>>>()
+        mGattCharacteristics = ArrayList<ArrayList<BluetoothGattCharacteristic>>()
+
+        // Loops through available GATT Services.
+        for (gattService in gattServices) {
+            val currentServiceData = HashMap<String, String>()
+            uuid = gattService.uuid.toString()
+            currentServiceData.put(
+                    LIST_NAME, SampleGattAttributes.lookup(uuid, unknownServiceString))
+            currentServiceData.put(LIST_UUID, uuid)
+            gattServiceData.add(currentServiceData)
+
+            val gattCharacteristicGroupData = ArrayList<HashMap<String, String>>()
+            val gattCharacteristics = gattService.characteristics
+            val charas = ArrayList<BluetoothGattCharacteristic>()
+
+            // Loops through available Characteristics.
+            for (gattCharacteristic in gattCharacteristics) {
+                charas.add(gattCharacteristic)
+                val currentCharaData = HashMap<String, String>()
+                uuid = gattCharacteristic.uuid.toString()
+                currentCharaData.put(
+                        LIST_NAME, SampleGattAttributes.lookup(uuid, unknownCharaString))
+                currentCharaData.put(LIST_UUID, uuid)
+                gattCharacteristicGroupData.add(currentCharaData)
+            }
+            mGattCharacteristics!!.add(charas)
+            gattCharacteristicData.add(gattCharacteristicGroupData)
+        }
+
+        val gattServiceAdapter = SimpleExpandableListAdapter(
+                this,
+                gattServiceData,
+                android.R.layout.simple_expandable_list_item_2,
+                arrayOf(LIST_NAME, LIST_UUID),
+                intArrayOf(android.R.id.text1, android.R.id.text2),
+                gattCharacteristicData,
+                android.R.layout.simple_expandable_list_item_2,
+                arrayOf(LIST_NAME, LIST_UUID),
+                intArrayOf(android.R.id.text1, android.R.id.text2)
+        )
+        mGattServicesList!!.setAdapter(gattServiceAdapter)
+    }
+
+    companion object {
+        private val TAG = DeviceControlActivity::class.java!!.getSimpleName()
+
+
+       @JvmField var EXTRAS_DEVICE_NAME = "DEVICE_NAME"
+       @JvmField var EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS"
+
+        private fun makeGattUpdateIntentFilter(): IntentFilter {
+            val intentFilter = IntentFilter()
+            intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED)
+            intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED)
+            intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED)
+            intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE)
+            return intentFilter
+        }
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+
+Kotlin BluetoothLeGatt
+===================================
+
+Intro
+-----
+This is a Kotlin conversion/fork of [Android BluetootLeGatt](https://github.com/googlesamples/android-BluetoothLeGatt).
+
+This was written for the ioTank101, which is project for the Hackster.io
+
+[2017 Co-Making the Future competition](https://www.hackster.io/contests/2017chinausyoungmakercompetition ).
+
+So far the DeviceControlActivity is converted to kotlin, making IoT development easy,
+
+and the project builds in Android Studio 3.0.
+
+
+License
+-------
+
+Copyright 2017 The Android Open Source Project, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
